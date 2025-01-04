2e50bc919e619ce27be5fe821c43ad9e4a39f61b
Erisiordian <hat.luka10@gmail.com>
2025-01-04 18:04:09 +0100

    zad4.asm

--- zadaća2/zad4.asm
+++ zadaća2/zad4.asm
@@ -70,3 +70,142 @@ M = 0;
 (END_HORIZONTAL)
 @SJECISTE
 D = M;
+@i
+M = D;
+
+@32
+D = A;
+
+@i
+M = M - D;
+
+@2
+D = A;
+
+@i
+A = M;
+M = D;
+
+@i
+M = M - 1;
+
+@k
+M = 0;
+
+(END_ROW)
+@i
+M = M + 1;
+
+@brojac
+M = 1;
+
+@j
+M = 0;
+
+(START_OKOMICA)
+	@k
+	D = M;
+	@64
+	D = D - A;
+	@END_OKOMICA
+	D; JGE
+	
+	@j
+	D = M;
+	@16
+	D = D - A;
+	@END_ROW
+	D; JGE
+	
+	@brojac
+	D = M;
+	M = M + D;
+	
+	@i
+	A = M;
+	M = M + D;
+	
+	@j
+	M = M + 1;
+	
+	@k
+	M = M + 1;
+	
+	@32
+	D = A;
+	@i
+	M = M - D;
+	
+	@START_OKOMICA
+	0; JMP
+	
+(END_OKOMICA)
+
+@k
+M = 0;
+
+@VRH
+D = M;
+
+@i
+M = D;
+M = M - 1;
+
+@2
+D = A;
+
+@VRH
+A = M;
+M = D;
+
+(END_ROWS)
+@i
+M = M + 1;
+
+@brojac
+M = 1;
+
+@j
+M = 0;
+
+(START_HYPOTENUSE)
+	@k
+	D = M;
+	@128
+	D = D - A;
+	@END_HYPOTENUSE
+	D; JGE
+	
+	@j
+	D = M;
+	@16
+	D = D - A;
+	@END_ROWS
+	D; JGE
+	
+	@brojac
+	D = M;
+	M = M + D;
+	
+	@i
+	A = M;
+	M = M + D;
+	
+	@j
+	M = M + 1;
+	
+	@k
+	M = M + 1;
+	
+	@32
+	D = A;
+	@i
+	M = M + D;
+	
+	@START_HYPOTENUSE
+	0; JMP
+(END_HYPOTENUSE)
+
+(END)
+@END
+0; JMP
